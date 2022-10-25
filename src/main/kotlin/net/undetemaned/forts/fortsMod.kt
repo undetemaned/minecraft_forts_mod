e93a3b56fb9aef8408e09b4bc0939cd53fb8983c
undetemend <annahannay@protonmail.com>
2022-10-25 11:31:05 +1100

    Initial commit

---
+++ src/main/kotlin/net/undetemaned/forts/fortsMod.kt
@@ -0,0 +1,18 @@
+package net.undetemaned.forts
+
+import org.quiltmc.loader.api.ModContainer
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+object fortsMod : ModInitializer {
+
+    const val MOD_ID: String = "forts"
+
+    @JvmField
+    val LOGGER: Logger = LoggerFactory.getLogger(MOD_ID)
+
+    override fun onInitialize(mod: ModContainer) {
+
+    }
+}
